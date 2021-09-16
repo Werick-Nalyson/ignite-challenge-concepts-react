916eb8c417691885ddb0fb14a7a267ab1581c2ee
Werick Nalyson <wericknalyson@gmail.com>
2021-09-15 22:23:14 -0300

    resolve challenge

--- src/components/TaskList.tsx
+++ src/components/TaskList.tsx
@@ -15,15 +15,33 @@ export function TaskList() {
   const [newTaskTitle, setNewTaskTitle] = useState('');
 
   function handleCreateNewTask() {
-    // Crie uma nova task com um id random, não permita criar caso o título seja vazio.
+    if (!newTaskTitle) {
+      return alert('Por favor, preencha o campo para inserir a tarefa!')
+    }
+
+    setTasks(oldTasks => [...oldTasks, {
+      id: Math.random(),
+      title: newTaskTitle,
+      isComplete: false
+    }])
+
+    setNewTaskTitle('')
   }
 
   function handleToggleTaskCompletion(id: number) {
-    // Altere entre `true` ou `false` o campo `isComplete` de uma task com dado ID
+    const taskIndex = tasks.findIndex(task => task.id === id)
+    
+    setTasks(oldTasks => {
+      oldTasks[taskIndex].isComplete = true
+
+      return [...oldTasks]
+    })
   }
 
   function handleRemoveTask(id: number) {
-    // Remova uma task da listagem pelo ID
+    const newTasks = tasks.filter(task => task.id !== id)
+
+    setTasks(newTasks)
   }
 
   return (
